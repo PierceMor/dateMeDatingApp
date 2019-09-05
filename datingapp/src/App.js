70b596bb8c42a7a137b5f78ac53a549e34108705
Pierce Morrill <piercemorrill91@gmail.com>
2019-09-04 20:00:19 -0600

    added basic routing

--- datingapp/src/App.js
+++ datingapp/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+// import Home from './pages/Home.js';
+// import CreateProfile from './pages/CreateProfile.js';
+// import Landing from './pages/Landing.js';
+// import Settings from './pages/Settings.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+class App extends Component {
+  render(){
+    return(
+      <Router>
+        <div>
+          <div className="main">
+            <Route exact path="/" component={Home} />
+            <Route exact path="/CreateProfile" component={CreateProfile} />
+            <Route exact path="/Landing" component={Landing} />
+            <Route exact path="/Settings" component={Settings} />
+
+          </div>
+        </div>
+      </Router>
+    )//return 
+  }//render
+} //App
 
 export default App;
