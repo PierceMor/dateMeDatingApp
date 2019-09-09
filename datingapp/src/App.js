9799957b4f3dd8a0cdffa74974eb3c10ccc3f99e
Pierce Morrill <piercemorrill91@gmail.com>
2019-09-09 10:08:11 -0600

    set up routes

--- datingapp/src/App.js
+++ datingapp/src/App.js
@@ -6,6 +6,8 @@ import Header from './components/Header.js';
 // import CreateProfile from './pages/CreateProfile.js';
 // import Landing from './pages/Landing.js';
 // import Settings from './pages/Settings.js';
+// import Admin from './pages/Admin.js';
+import Signin from './pages/SignIn.js';
 
 
 
@@ -20,9 +22,10 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route exact path="/CreateProfile" component={CreateProfile} />
             <Route exact path="/Landing" component={Landing} />
+            <Route exact path="/Admin" component={Admin} />
             <Route exact path="/Settings" component={Settings} />
-             */}
-
+            */}
+            <Route exact path="/Signin" component={Signin} />
           </div>
         </div>
       </Router>
--- datingapp/src/pages/SignIn.js
+++ datingapp/src/pages/SignIn.js
@@ -1,1 +1,39 @@
 //allows for a user to sign in to their already created profile
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const classes = makeStyles(theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    testField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: 200,
+    },
+}));
+
+export default function SignIn() {
+
+    return (
+        <div className={classes.container}>
+            <TextField
+                label="Email"
+                id="SignInEmail"
+                type='email'
+                variant='filled'
+                className={classes.TextField}
+            />
+            <TextField
+                label="Password"
+                id="SignInPassword"
+                type='password'
+                variant='filled'
+                className={classes.TextField}
+            />
+        </div>
+    )
+}
